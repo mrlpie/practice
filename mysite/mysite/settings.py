57dd5a8a6712769df161c0d1e8d190f14e879434
Maria Legoshova <maruslic2005@gmail.com>
2022-06-10 10:29:17 +0300

    second

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LANGUAGE_CODE = 'ru-ru'
+TIME_ZONE = 'Europe/Moscow'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
